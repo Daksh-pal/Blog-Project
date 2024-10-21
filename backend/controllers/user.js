97198bee611aaf881cac4b3fd723a233dd5af551
Daksh-Pal <pal.daksh22@gmail.com>
2024-10-21 13:29:00 +0530

    Created the User model and login , registering backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,46 +1,90 @@
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
+import bcrypt, { hash } from 'bcrypt'
 
 export const register = async (req,res) => {
+    
     const {firstName , lastName, email , password} = req.body;
-    console.log("Checkpoint 1");
     
     try {
-        console.log("Checkpoint 2");
         if(!firstName || !lastName || !email || !password){
             return res.status(400).json({error : "Please fill all the fields"});
         }
         
-        console.log("Checkpoint 3");
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         
-        // if(!regex.test(email)){
-        //     console.log("Checkpoint 4");
-        //     return res.status(400).json({error : "Please enter valid Email"});
-        // }
+        if (!validateEmail(email)) {
+            console.log("Checkpoint 3");
+            return res.status(400).json({ error: "Please enter a valid email address" });
+        }
         
-        console.log("Checkpoint 5");
         const user = await User.findOne({email});
         
         if(user){
-            console.log("Checkpoint 6");
             return res.status(400).json({Error : "User already registered."})
         }
-        console.log("Checkpoint 7");
-    
+        
         const hashedPassword = await bcrypt.hash(password,10);
-    
+        
         const newUser = new User({firstName,lastName,email,password: hashedPassword});
         await newUser.save();
+
         const token = generateToken(newUser._id);
+
         return res.status(200).json({Message : "User Registered Successfully" , token});
+
     } catch (error) {
-        console.log("Inside catch  block")
+
+        console.log("Inside catch  block in register function")
         return res.status(500).json({Error : error});
+
     }
 }
 
-const generateToken = async (id) => {
-    const token = jwt.sign(id , process.env.JWT_KEY , {expiresIn : '1h'})
-    return token;
+
+export const login = async (req,res) => {
+
+    const {email , password} = req.body;
+    
+    try {
+        if(!email || !password){
+            return res.status(400).json({error : "Please fill all the fields"});
+        }
+        
+        
+        if (!validateEmail(email)) {
+            console.log("Checkpoint 3");
+            return res.status(400).json({ error: "Please enter a valid email address" });
+        }
+        
+        const user = await User.findOne({email});
+        
+        if(!user){
+            return res.status(400).json({Error : "No such User is registered."})
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: "Invalid Password" });
+        }
+
+        const token = generateToken(user._id);
+
+        return res.status(200).json({Message : "User Logged in Successfully" , token});
+
+    } catch (error) {
+
+        console.log("Inside catch  block in register function")
+        return res.status(500).json({Error : error});
+
+    }
+}
+
+const generateToken =  (id) => {
+    return jwt.sign({id} , process.env.JWT_KEY , {expiresIn : '1h'})
+}
+
+const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 }
